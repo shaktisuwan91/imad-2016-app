46c73ea0f6fd63e08c90a055fbe6dc62cf6b75f7
shaktisuwan91 <shaktisuwan91@gmail.com>
2016-10-01 10:08:21 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/article-one',function (req, res) {
+app.get('/Article-one',function (req, res) {
  res.sendFile(path.join(__dirname, 'ui', 'Articleone.html')); 
 });
 app.get('/article-two',function (req, res) {
