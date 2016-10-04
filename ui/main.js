deb7145cda01f4bcf8d52332f045842672020a0c
shaktisuwan91 <shaktisuwan91@gmail.com>
2016-10-04 13:25:59 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,6 +4,11 @@ var element=document.getElementById("mainId");
 element.innerHTML="Bharat Mata Ki Jai";
 //move the image
 var img = document.getElementById("mee");
+marginLeft = 0;
+function moveRight(){
+    marginLeft = marginLeft + 5;
+    img.style.marginLeft =  marginLeft + 'px'; 
+}
 img.onclick = function() {
-    img.style.marginLeft = '100px';
+    var interval = setInteval(moveRight,50);
 };
