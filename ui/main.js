4590f03ca7afe0e7be509ba1cad8c903e64b4b58
shaktisuwan91 <shaktisuwan91@gmail.com>
2016-10-04 12:45:49 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,5 @@
 console.log('Loaded!');
 //change the text 
-var element=document.getElementById('mainId');
+var element=document.getElementById("mainId");
 element.innerHTML="Bharat Mata Ki Jai";
 
