aeafb119744ccf590ac943f2b8fc8070cbabcc89
shaktisuwan91 <shaktisuwan91@gmail.com>
2016-10-04 11:16:54 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -81,6 +81,8 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/:articleName',function (req, res) {
+    //articleName = article-one
+    //articles[articleName] = {} 
 var articleName = req.params.articleName;    
  res.send(createTemplate(articles[articleName])); 
 });
