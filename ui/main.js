50a506d718670b3244e3428ed9838bce8fb6a068
shaktisuwan91 <shaktisuwan91@gmail.com>
2016-10-07 03:49:16 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,10 +9,7 @@ function moveRight(){
     marginLeft = marginLeft + 5;
     img.style.marginLeft =  marginLeft + 'px'; 
 }
-marginLeft = 0;
-function moveRight() {
-    marginLeft = marginLeft + 'px';
-}
+
 img.onclick = function() {
     var interval = setInterval(moveRight,50);
 };
